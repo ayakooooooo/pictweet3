322b7a48cb86d373f833d27f494bb76187449403
ayakooooooo <itoshunitoshunitoshun@gmail.com>
2024-01-20 20:16:28 +0900

    newアクションをコントローラーに定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,4 +3,9 @@ class TweetsController < ApplicationController
     @tweets = Tweet.all
     #allメソッドを使用して、tweetsテーブルすべてのレコードをインスタンス変数に代入し、ビューに受け渡し
   end
+  def new
+    @tweet = Tweet.new
+    #投稿画面へ遷移（画面移動）する
+    #Tweetクラスのインスタンス変数を生成
+  end
 end
