3462f924aeb5d6b490ee46a816d70f31be04576b
ayakooooooo <itoshunitoshunitoshun@gmail.com>
2024-01-20 20:34:08 +0900

    createアクションをコントローラーに定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,4 +8,17 @@ class TweetsController < ApplicationController
     #投稿画面へ遷移（画面移動）する
     #Tweetクラスのインスタンス変数を生成
   end
+  def create
+    Tweet.create(tweet_params)
+    #下のプライベートメソッドの呼び出し
+    redirect_to '/'
+    #保存後はredirect_toを使用してトップページ表示
+  end
+
+  private
+  def tweet_params
+    #ストロングパラメーター定義
+    params.require(:tweet).permit(:name, :image, :text)
+    #特定のキーを受け取るように制限
+  end
 end
