82d8165d0fafd71dd6b13a166893f1e2c652d239
ayakooooooo <itoshunitoshunitoshun@gmail.com>
2024-01-22 18:58:15 +0900

    showアクションをコントローラーに定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -31,6 +31,10 @@ class TweetsController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
   private
   def tweet_params
     #ストロングパラメーター定義
