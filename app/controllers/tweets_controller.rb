c73a053672bdd547f45cf7e8e26b1d71416838a9
ayakooooooo <itoshunitoshunitoshun@gmail.com>
2024-01-22 18:17:57 +0900

    updateアクションをコントローラーに定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -24,7 +24,13 @@ class TweetsController < ApplicationController
   def edit
     @tweet = Tweet.find(params[:id])
   end
-  
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+    redirect_to root_path
+  end
+
   private
   def tweet_params
     #ストロングパラメーター定義
