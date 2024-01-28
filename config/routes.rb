8b374ea86598693bbb54a7c95581afe04d32cf93
ayakooooooo <itoshunitoshunitoshun@gmail.com>
2024-01-28 14:44:15 +0900

    usersコントローラーのshowアクションを動かせるようにルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index'
   resources :tweets
+  resources :users, only: :show #マイページを表示する際にはusersコントローラーのshowアクションを動かせるように
+  # /users/:idのパスでリクエストした際にusers_controller.rbのshowアクションを実行する
 end
