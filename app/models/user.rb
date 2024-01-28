7c2e8bb0c19a9bee0e8943a8d3eea0f19087d497
ayakooooooo <itoshunitoshunitoshun@gmail.com>
2024-01-28 14:39:11 +0900

    アソシエーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
+  has_many :tweets
+  #1人のユーザーは複数の投稿を所有
+  #has_manyメソッド userと他のモデルとの間に「1対多」のつながりがあることを示す
 end
