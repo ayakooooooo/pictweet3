35f1c24d8991323060fdb7f8a0c8c05bd288e9a8
ayakooooooo <itoshunitoshunitoshun@gmail.com>
2024-01-29 16:19:19 +0900

    ログイン中ユーザーのマイページを表示するため編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+  def show
+    #ログイン中ユーザーのマイページ
+    @nickname = current_user.nickname #ニックネーム
+    @tweets = current_user.tweets #ログイン中のユーザーのツイート投稿
+  end
 end
